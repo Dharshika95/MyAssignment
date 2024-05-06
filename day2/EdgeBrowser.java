32f1c761634b56e3e6a733282f9007f746e218e1
Dharshika95 <168316179+Dharshika95@users.noreply.github.com>
2024-05-06 21:21:06 +0530

    Add files via upload
    Day 2

---
+++ day2/EdgeBrowser.java
@@ -0,0 +1,11 @@
+package week1.day2;
+
+public class EdgeBrowser {
+	public static void main(String[] args) {
+		
+		Browser bm =new Browser();
+		System.out.println(bm.launchBrowser("google chrome"));
+		bm.loadUrl();
+	}
+
+}
